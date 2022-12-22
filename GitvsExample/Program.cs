8fadd1fa573c708a10bd6d255652678338853f3d
RahmanVahabov <rahmanrv@code.edu.az.com>
2022-12-22 19:40:55 +0400

    repo1

--- GitvsExample/Program.cs
+++ GitvsExample/Program.cs
@@ -4,6 +4,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Change");
+        Console.WriteLine("Hello from dev");
+        Console.WriteLine("hello P329");
     }
 }
 
